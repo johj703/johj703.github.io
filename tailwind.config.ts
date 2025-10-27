e770dd4cb95b895bff9878cfa27b7ce6393ac20b
Jo <johj703@nate.com>
2025-10-27 18:52:57 +0900

    🔧fix: 타입스크립트 기반으로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class", // 클래스 기반 다크모드 활성화
+  darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        // 다크모드용 커스텀 컬러 추가 기능
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
+export default config;
