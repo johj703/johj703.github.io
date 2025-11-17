249be5bf7292e326db572690e3b8c65156e4bea7
Jo <johj703@nate.com>
2025-11-17 17:55:18 +0900

    🔧fix: persist 안에 있는 객체들 중 section을 sections로 변경

--- src/stores/useNavigationStore.ts
+++ src/stores/useNavigationStore.ts
@@ -2,22 +2,21 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface NavigationStore {
-    activeSection: string,
-    setActiveSection: (section: string) => void;
-    section: string[];
+  activeSection: string;
+  setActiveSection: (section: string) => void;
+  section: string[];
 }
 
 export const useNavigationStore = create<NavigationStore>()(
-    persist(
-        (set) => ({
-            activeSection: "here",
-            section: ["here", "about", "skills", "projects", "contact"],
-            setActiveSection: (section: string) =>
-                set({ activeSection: section })
-        }),
-        {
-            name: "navigation-storage",
-            partialize: (state) => ({ activeSection: state.activeSection}),
-        }
-    )
+  persist(
+    (set) => ({
+      activeSection: "hero",
+      sections: ["here", "about", "skills", "projects", "contact"],
+      setActiveSection: (section: string) => set({ activeSection: section }),
+    }),
+    {
+      name: "navigation-storage",
+      partialize: (state) => ({ activeSection: state.activeSection }),
+    }
+  )
 );
